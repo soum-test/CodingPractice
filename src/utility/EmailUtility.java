71f71efe7152c1b003ca66ef2f95ae2c1ab7da13
Soumitra Dev Burman <sdburman07@gmail.com>
2021-11-11 21:24:53 +0530

    more email utility commands

--- src/utility/EmailUtility.java
+++ src/utility/EmailUtility.java
@@ -7,13 +7,18 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
 import microsoft.exchange.webservices.data.core.ExchangeService;
+import microsoft.exchange.webservices.data.core.PropertySet;
 import microsoft.exchange.webservices.data.core.enumeration.misc.ExchangeVersion;
+import microsoft.exchange.webservices.data.core.enumeration.property.BodyType;
 import microsoft.exchange.webservices.data.core.enumeration.property.WellKnownFolderName;
 import microsoft.exchange.webservices.data.core.service.folder.Folder;
+import microsoft.exchange.webservices.data.core.service.item.EmailMessage;
 import microsoft.exchange.webservices.data.core.service.item.Item;
+import microsoft.exchange.webservices.data.core.service.schema.EmailMessageSchema;
 import microsoft.exchange.webservices.data.credential.ExchangeCredentials;
 import microsoft.exchange.webservices.data.credential.WebCredentials;
 import microsoft.exchange.webservices.data.property.complex.EmailAddress;
+import microsoft.exchange.webservices.data.property.complex.EmailAddressCollection;
 import microsoft.exchange.webservices.data.property.complex.FolderId;
 import microsoft.exchange.webservices.data.property.complex.Mailbox;
 import microsoft.exchange.webservices.data.search.FolderView;
@@ -240,7 +245,58 @@ public class EmailUtility {
 		this.folder = this.getUserDefinedFolder(sUserDefinedFolderName);
 	}
 	
+//	public string getEmailComponent
 	
+	public String getEmailComponent(Item mailItem, EmailComponent component) {
+		String sValue = "";
+		
+		try {
+			mailItem.load();
+			EmailMessage email = EmailMessage.bind(this.service, mailItem.getId(), PropertySet.FirstClassProperties);
+			
+			switch(component) {
+				case SUBJECT : sValue = mailItem.getSubject();
+								break;
+				case BODY_TEXT: PropertySet propset = new PropertySet(EmailMessageSchema.Body);
+								propset.setRequestedBodyType(BodyType.Text);
+								Item item = Item.bind(service, this.emailItem.getId(), propset);
+								item.load(propset);
+								EmailMessage emailMessage = EmailMessage.bind(service, item.getId(),propset);
+								sValue = emailMessage.getBody().toString();
+								break;
+				case TO_ADDRESS: sValue = this.getEmailAddressListString(email.getReplyTo());
+								 break;
+				case TO_NAMES:	sValue = email.getDisplayTo();
+								break;
+								
+				case CC_ADDRESS: sValue = this.getEmailAddressListString(email.getCcRecipients());
+								 break;
+								 
+				case CC_NAMES: sValue = email.getDisplayCc();
+								break;
+				case BCC_ADDRESS: sValue = this.getEmailAddressListString(email.getBccRecipients());
+									break;
+				case FROM_ADDRESS: sValue = email.getFrom().getAddress();
+									break;
+				case FROM_NAME: sValue = email.getSender().getName();
+								break;
+				case CREATED_DATETIME: sValue = email.getDateTimeCreated().toString();
+										break;
+				case SENT_DATETIME: sValue = email.getDateTimeSent().toString();
+									break;
+				case RECEIVED_DATETIME: sValue = email.getDateTimeReceived().toString();
+										break;
+				default:
+					break;
+							
+			}
+			
+		}catch (Exception e) {
+			// TODO: handle exception
+		}
+		
+		return sValue;
+	}
 	/**
 	 * Method to get HTML body of the email
 	 * @param mailItem
@@ -290,6 +346,16 @@ public class EmailUtility {
 		}
 		
 		return null;
+	}
+	
+	public String getEmailAddressListString(EmailAddressCollection collection) {
+		String sAddresses = "";
+		
+		for (EmailAddress emailAddress : collection) {
+			sAddresses += emailAddress.getAddress() + ";";
+		}
+		
+		return sAddresses;
 	}
 	
 	/**
